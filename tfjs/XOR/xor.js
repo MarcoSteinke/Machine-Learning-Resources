e5f72489b792cb57c145ddd82b88b2b162e5b3db
maste150hhu <mastremolten2@gmail.com>
2021-07-04 19:15:29 +0200

    push latest state

--- tfjs/XOR/xor.js
+++ tfjs/XOR/xor.js
@@ -1,27 +1,19 @@
-// Define a model for linear regression.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [2]}));
+async function runModel() {
 
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+// Build and compile model.
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 2, inputShape: [1]}));
+model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
 
 // Generate some synthetic data for training.
-const xs = tf.data.array([
-    tf.tensor([0,0]),
-    tf.tensor([0,1]),
-    tf.tensor([1,0]),
-    tf.tensor([1,1]),
-]);
-const ys = tf.data.array([
-    tf.tensor([0]),
-    tf.tensor([1]),
-    tf.tensor([1]),
-    tf.tensor([0]),
-]);
+const xs = tf.tensor([[1,0]]);
+const ys = tf.tensor([1]);
 
-// Train the model
-model.fit(xs,ys).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    // Open the browser devtools to see the output
-    model.predict(tf.tensor[1,0]).print();
-});
+// Train model with fit().
+await model.fit(xs, ys, {epochs: 10});
+
+// Run inference with predict().
+model.predict(tf.tensor2d([0,1])).print();
+}
+
+runModel();
