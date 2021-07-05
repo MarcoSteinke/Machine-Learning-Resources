18c4bd392f13da5a3737cc9da3db24bfe48e88de
maste150hhu <mastremolten2@gmail.com>
2021-07-05 12:25:51 +0200

    wrap training and prediction in async function

--- tfjs/XOR/xor.js
+++ tfjs/XOR/xor.js
@@ -1,4 +1,37 @@
-async function runModel() {
+function createDataPoint(x, y, label) {
+    return {
+        x: x,
+        y: y,
+        label: label
+    };
+}
+
+async function train(model, input) {
+
+    
+
+    // Train model with fit().
+    for(let dataPoint in input) {
+        await model.fit(tf.tensor2d([[input[dataPoint].x, input[dataPoint].y]]), tf.tensor2d([input[dataPoint].label], [1,1]));
+    }
+
+    console.log("Training complete");
+
+    
+}
+
+async function runModel(model, input) {
+    await train(model, input);
+
+    // Check model for correct results:
+
+    input.forEach(
+        dataPoint => {
+            console.log(`Model output for [${dataPoint.x}, ${dataPoint.y}]`);
+            model.predict(tf.tensor2d([dataPoint.x, dataPoint.y], [1,2])).print();
+        }
+    )
+}
 
 // Build and compile model.
 const model = tf.sequential();
@@ -6,21 +39,12 @@ model.add(tf.layers.dense({units: 1, inputShape: [2]}));
 model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
 
 // Generate some synthetic data for training.
-const xs = tf.tensor2d([[1,0]]);
-const ys = tf.tensor2d([1], [1,1]);
+const input = [
+    createDataPoint(0,0,0),
+    createDataPoint(0,1,1),
+    createDataPoint(1,0,1),
+    createDataPoint(1,1,0),
+]
 
-// Train model with fit().
-await model.fit(xs, ys, {epochs: 2000});
+runModel(model, input);
 
-// Run inference with predict().
-console.log("Model output for [0,0]");
-model.predict(tf.tensor2d([0,0], [1,2])).print();
-console.log("Model output for [0,1]");
-model.predict(tf.tensor2d([0,1], [1,2])).print();
-console.log("Model output for [1,0]");
-model.predict(tf.tensor2d([1,0], [1,2])).print();
-console.log("Model output for [1,1]");
-model.predict(tf.tensor2d([1,1], [1,2])).print();
-}
-
-runModel();
