391e9d73e1125a0b0cf2edfa8d3a4a399ad4c48b
maste150hhu <mastremolten2@gmail.com>
2021-09-20 11:00:52 +0200

    add template

---
+++ implementation/text-prediction/Bigram.js
@@ -0,0 +1,16 @@
+class Bigram {
+
+    previous
+    next
+    probability
+
+    constructor(previous, next, probability) {
+        this.previous = previous;
+        this.next = next;
+        this.probability = probability;
+    } 
+
+    findProbability(input) {
+        // run some logic
+    }
+}
