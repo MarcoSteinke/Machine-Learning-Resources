a7295b812801e13437c2f6111115e12ba489f839
maste150hhu <mastremolten2@gmail.com>
2021-10-10 11:39:27 +0200

    format analyse.js

--- implementation/political-parties/analyse.js
+++ implementation/political-parties/analyse.js
@@ -1,11 +1,11 @@
 class Vector {
 
     constructor(values) {
-      this.values = values;
+        this.values = values;
     }
-  
+
     getDimension = () => this.values.length;
-  
+
     getValues = () => this.values;
 
     getTotalDifference(anotherVector) {
@@ -16,21 +16,21 @@ class Vector {
 
         return diff;
     }
-  }
+}
   
-  let tmpParties = [...document.querySelector("#aussen > div.rand10px > div:nth-child(9) > table > tbody > tr:nth-child(1)").cells].map(c => c.innerText).slice(2, [...document.querySelector("#aussen > div.rand10px > div:nth-child(9) > table > tbody > tr:nth-child(1)").cells].length).map(party => party.replace("\n", ""))
-  
-  let tmpPartyObjects = [];
-  tmpParties.forEach(party => { tmpPartyObjects.push({name: party, value: []}) });
-  
-  [...[...document.querySelectorAll(".tabscroll")].map(table => [...table.firstElementChild.rows]).map(array => array.splice(1, array.length-2))].flat().forEach((row) => {
-   [...row.cells].splice(2).forEach(cell => { 
-     if(cell.innerText == 'Ja') { tmpPartyObjects[cell.cellIndex-2].value.push(1); }
-     else if(cell.innerText == 'Nein') { tmpPartyObjects[cell.cellIndex-2].value.push(-1); }
-     else { tmpPartyObjects[cell.cellIndex-2].value.push(0); }
-   } );
-  });
-  
-  tmpPartyObjects = tmpPartyObjects.map(party => {return {name: party.name, value: new Vector(party.value)}})
-  
-  tmpPartyObjects
+let tmpParties = [...document.querySelector("#aussen > div.rand10px > div:nth-child(9) > table > tbody > tr:nth-child(1)").cells].map(c => c.innerText).slice(2, [...document.querySelector("#aussen > div.rand10px > div:nth-child(9) > table > tbody > tr:nth-child(1)").cells].length).map(party => party.replace("\n", ""))
+
+let tmpPartyObjects = [];
+tmpParties.forEach(party => { tmpPartyObjects.push({name: party, value: []}) });
+
+[...[...document.querySelectorAll(".tabscroll")].map(table => [...table.firstElementChild.rows]).map(array => array.splice(1, array.length-2))].flat().forEach((row) => {
+    [...row.cells].splice(2).forEach(cell => { 
+        if(cell.innerText == 'Ja') { tmpPartyObjects[cell.cellIndex-2].value.push(1); }
+        else if(cell.innerText == 'Nein') { tmpPartyObjects[cell.cellIndex-2].value.push(-1); }
+        else { tmpPartyObjects[cell.cellIndex-2].value.push(0); }
+    } );
+});
+
+tmpPartyObjects = tmpPartyObjects.map(party => {return {name: party.name, value: new Vector(party.value)}})
+
+tmpPartyObjects
